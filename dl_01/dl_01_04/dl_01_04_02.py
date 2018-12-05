f83b3b5945435ab58e317f7c23da505467939e47
GuoChang <827789895@qq.com>
2018-12-05 20:28:22 +0800

    深层神经网络方法构建

--- dl_01/dl_01_04/dl_01_04_02.py
+++ dl_01/dl_01_04/dl_01_04_02.py
@@ -190,10 +190,10 @@ def start():
     # plt.show()
     # print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
 
-    # # Explore your dataset
-    # m_train = train_x_orig.shape[0]
-    # num_px = train_x_orig.shape[1]
-    # m_test = test_x_orig.shape[0]
+    # Explore your dataset
+    m_train = train_x_orig.shape[0]
+    num_px = train_x_orig.shape[1]
+    m_test = test_x_orig.shape[0]
     #
     # print("Number of training examples: " + str(m_train))
     # print("Number of testing examples: " + str(m_test))
@@ -228,8 +228,26 @@ def start():
     # layers_dims = [12288, 7, 1]  # 5-layer model
     layers_dims = [12288, 20, 7, 5, 1]  # 5-layer model
     parameters = L_layer_model(train_x, train_y, layers_dims, learning_rate=0.0075, num_iterations=2500, print_cost=True)
-    predictions_train = predict(train_x, train_y, parameters)
-    predictions_test = predict(test_x, test_y, parameters)
+    pred_train = predict(train_x, train_y, parameters)
+    pred_test = predict(test_x, test_y, parameters)
+
+    print_mislabeled_images(classes, test_x, test_y, pred_test)
+
+    # Test with your own image
+    ## START CODE HERE ##
+    my_image = "cat2.jpg"  # change this to the name of your image file
+    my_label_y = [1]  # the true class of your image (1 -> cat, 0 -> non-cat)
+    ## END CODE HERE ##
+
+    fname = r'../dl_01_02/datasets/' + my_image
+    image = np.array(ndimage.imread(fname, flatten=False))
+    my_image = scipy.misc.imresize(image, size=(num_px, num_px)).reshape((num_px * num_px * 3, 1))
+    my_predicted_image = predict(my_image, my_label_y, parameters)
+
+    plt.imshow(image)
+    print("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[
+        int(np.squeeze(my_predicted_image)), ].decode("utf-8") + "\" picture.")
+    plt.show()
 
 
 if __name__ == '__main__':
--- dl_01/dl_01_04/dnn_app_utils_v2.py
+++ dl_01/dl_01_04/dnn_app_utils_v2.py
@@ -437,12 +437,13 @@ def print_mislabeled_images(classes, X, y, p):
     """
     a = p + y
     mislabeled_indices = np.asarray(np.where(a == 1))
-    plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
+    plt.rcParams['figure.figsize'] = (40.0, 40.0)  # set default size of plots
     num_images = len(mislabeled_indices[0])
     for i in range(num_images):
         index = mislabeled_indices[1][i]
         
         plt.subplot(2, num_images, i + 1)
-        plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
+        plt.imshow(X[:, index].reshape(64, 64, 3), interpolation='nearest')
         plt.axis('off')
         plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
+    plt.show()
