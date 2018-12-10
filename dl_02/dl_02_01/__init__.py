a01fc02fa446044bbed3609c78e18fbc1120b645
GuoChang <827789895@qq.com>
2018-12-10 11:50:57 +0800

    初始化参数研究

---
+++ dl_02/dl_02_01/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/10 10:27
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : __init__.py.py
+# @Software: PyCharm
