02831eadb6c861a034e993d4886415d9353b80f0
GuoChang <827789895@qq.com>
2018-12-12 17:33:26 +0800

    小批量梯度下降、momentum小批量梯度下降、adam小批量梯度下降

---
+++ dl_02/dl_02_02/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/11 17:43
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : __init__.py.py
+# @Software: PyCharm
